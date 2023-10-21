666df3d388a41029364c5a7c54f5038d90d18fd2
Daryna Holikova <dgolikova0@gmail.com>
2023-10-21 23:56:40 +0100

    finilizing

--- src/redux/store.jsx
+++ src/redux/store.jsx
@@ -1,12 +1,27 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { persistedReducer } from './reducer';
-import { persistStore } from 'redux-persist';
+import {
+    persistStore ,
+    persistReducer,
+    FLUSH,
+    REHYDRATE,
+    PAUSE,
+    PERSIST,
+    PURGE,
+    REGISTER,
+  } from 'redux-persist'
 
 export const store = configureStore({
   reducer: {
     phonebook: persistedReducer,
   },
-  blacklist: ['filter']
+  blacklist: ['filter'],
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
 });
 
 
